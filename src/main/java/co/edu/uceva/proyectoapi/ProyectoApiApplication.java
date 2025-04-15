0b81254187dba4a144ce29f4eeb133fbd6136f10
Angie Lorena González <angie.gonzales01@uceva.edu.co>
2025-04-15 15:47:49 -0500

    Subir todos los cambios finales sin incluir .env

--- src/main/java/co/edu/uceva/proyectoapi/ProyectoApiApplication.java
+++ src/main/java/co/edu/uceva/proyectoapi/ProyectoApiApplication.java
@@ -1,5 +1,6 @@
 package co.edu.uceva.proyectoapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoApiApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+
         SpringApplication.run(ProyectoApiApplication.class, args);
     }
 
