cc2ae3454d7336fa515e9e6691db99e46c76bb85
sebastian trujillo <sebastian.trujillo02@uceva.edu.co>
2025-04-16 12:10:48 -0500

    se corrigio el problema de las variables con el conenedero modificando el metodo main del proyecto

--- src/main/java/co/edu/uceva/proyectoapi/ProyectoApiApplication.java
+++ src/main/java/co/edu/uceva/proyectoapi/ProyectoApiApplication.java
@@ -8,11 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoApiApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
 
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        // Solo cargar el .env si las variables no vienen definidas
+        if (System.getenv("DB_URL") == null || System.getenv("DB_USER") == null || System.getenv("DB_PASS") == null) {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+            System.setProperty("DB_USER", dotenv.get("DB_USER"));
+            System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        }
 
         SpringApplication.run(ProyectoApiApplication.class, args);
     }
